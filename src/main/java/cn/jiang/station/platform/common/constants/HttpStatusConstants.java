339f335943d7a7dd978930ff7284ffb1efcb329c
jshk123 <jshk123@qq.om>
2020-03-29 14:31:59 +0800

    update

---
+++ src/main/java/cn/jiang/station/platform/common/constants/HttpStatusConstants.java
@@ -0,0 +1,21 @@
+package cn.jiang.station.platform.common.constants;
+
+public enum HttpStatusConstants {
+    BAD_GATEWAY(502, "从上游服务器接收到无效的响应");
+
+    private int status;
+    private String content;
+
+    HttpStatusConstants(int status, String content) {
+        this.status = status;
+        this.content = content;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
