f59b498690c1425c5c811dbedcbe7013aeb174e1
jshk123 <jshk123@qq.om>
2020-05-05 22:22:33 +0800

    update

--- src/main/java/cn/jiang/station/platform/common/constants/HttpStatusConstants.java
+++ src/main/java/cn/jiang/station/platform/common/constants/HttpStatusConstants.java
@@ -1,21 +1,27 @@
 package cn.jiang.station.platform.common.constants;
 
+import lombok.Getter;
+
+@Getter
 public enum HttpStatusConstants {
     BAD_GATEWAY(502, "从上游服务器接收到无效的响应");
 
     private int status;
     private String content;
 
+    public static HttpStatusConstants getHttpStatusConstants(int status) {
+        HttpStatusConstants[] values = HttpStatusConstants.values();
+        for (HttpStatusConstants httpStatusConstants : values) {
+            if (status == httpStatusConstants.getStatus()) {
+                return httpStatusConstants;
+            }
+        }
+        return null;
+    }
+
     HttpStatusConstants(int status, String content) {
         this.status = status;
         this.content = content;
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
